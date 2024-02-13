f638197f4b7abb0ee6f0c6f08cc2809b0fb8ec0d
David Shelton <david.faria012@gmail.com>
2024-02-12 22:14:55 -0300

    test:(FindUser use case): Add new test cases Find user by id, but passing no id; Find user by name, but passing no name; Find user by id and name, but passing no id; Find user by id and name, but passing no name; Find user by id and name, but passing no id or name

--- src/data/find_user/find_test.py
+++ src/data/find_user/find_test.py
@@ -27,6 +27,28 @@ def test_by_id():
     assert response1["Data"]
 
 
+def test_by_id_no_id():
+    """Testing Find User use case by_id method, but passing no id"""
+    user_repo = UserRepositorySpy()
+    find_user = FindUser(user_repo)
+
+    # Fake data
+    attributes = {"user_id": None}
+
+    # Select user by id:
+    response1 = find_user.by_id(attributes["user_id"])
+
+    print(user_repo.select_user_params)
+    print(response1)
+
+    # Test inputs
+    assert not user_repo.select_user_params
+
+    # Test outputs
+    assert response1["Success"] is False
+    assert not response1["Data"]
+
+
 def test_by_name():
     """Testing Find User use case"""
     user_repo = UserRepositorySpy()
@@ -47,6 +69,28 @@ def test_by_name():
     # Test outputs
     assert response1["Success"] is True
     assert response1["Data"]
+
+
+def test_by_name_no_name():
+    """Testing Find User use case"""
+    user_repo = UserRepositorySpy()
+    find_user = FindUser(user_repo)
+
+    # Fake data
+    attributes = {"user_name": None}
+
+    # Select user by name:
+    response1 = find_user.by_name(attributes["user_name"])
+
+    print(user_repo.select_user_params)
+    print(response1)
+
+    # Test inputs
+    assert not user_repo.select_user_params
+
+    # Test outputs
+    assert response1["Success"] is False
+    assert not response1["Data"]
 
 
 def test_by_id_and_name():
@@ -70,3 +114,72 @@ def test_by_id_and_name():
     # Test outputs
     assert response1["Success"] is True
     assert response1["Data"]
+
+
+def test_by_id_and_name_no_id():
+    """Testing Find User use case"""
+    user_repo = UserRepositorySpy()
+    find_user = FindUser(user_repo)
+
+    # Fake data
+    attributes = {"user_id": None, "user_name": faker.name()}
+
+    # Select user by name:
+    response1 = find_user.by_id_and_name(attributes["user_id"], attributes["user_name"])
+
+    print(user_repo.select_user_params)
+    print(response1)
+
+    # Test inputs
+    assert not user_repo.select_user_params
+    assert not user_repo.select_user_params
+
+    # Test outputs
+    assert response1["Success"] is False
+    assert not response1["Data"]
+
+
+def test_by_id_and_name_no_name():
+    """Testing Find User use case"""
+    user_repo = UserRepositorySpy()
+    find_user = FindUser(user_repo)
+
+    # Fake data
+    attributes = {"user_id": faker.random_number(digits=5), "user_name": None}
+
+    # Select user by name:
+    response1 = find_user.by_id_and_name(attributes["user_id"], attributes["user_name"])
+
+    print(user_repo.select_user_params)
+    print(response1)
+
+    # Test inputs
+    assert not user_repo.select_user_params
+    assert not user_repo.select_user_params
+
+    # Test outputs
+    assert response1["Success"] is False
+    assert not response1["Data"]
+
+
+def test_by_id_and_name_no_name_or_id():
+    """Testing Find User use case"""
+    user_repo = UserRepositorySpy()
+    find_user = FindUser(user_repo)
+
+    # Fake data
+    attributes = {"user_id": None, "user_name": None}
+
+    # Select user by name:
+    response1 = find_user.by_id_and_name(attributes["user_id"], attributes["user_name"])
+
+    print(user_repo.select_user_params)
+    print(response1)
+
+    # Test inputs
+    assert not user_repo.select_user_params
+    assert not user_repo.select_user_params
+
+    # Test outputs
+    assert response1["Success"] is False
+    assert not response1["Data"]

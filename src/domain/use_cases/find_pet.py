bd9beeffee02cc1ebc477f627cb0cfe920292579
David Shelton <david.faria012@gmail.com>
2024-02-12 21:54:41 -0300

    refactor(FindPet interface/FindUser interface): Change class methods to common methods

--- src/domain/use_cases/find_pet.py
+++ src/domain/use_cases/find_pet.py
@@ -6,20 +6,17 @@ from src.domain.models import Pets
 class FindPet(ABC):
     """Interface to FindPet use case"""
 
-    @classmethod
     @abstractmethod
-    def by_id(cls, pet_id: int) -> Dict[bool, List[Pets]]:
+    def by_id(self, pet_id: int) -> Dict[bool, List[Pets]]:
         """Specific case"""
         raise NotImplementedError("Should implement method: by_id")
 
-    @classmethod
     @abstractmethod
-    def by_user_id(cls, user_id: str) -> Dict[bool, List[Pets]]:
+    def by_user_id(self, user_id: str) -> Dict[bool, List[Pets]]:
         """Specific case"""
         raise NotImplementedError("Should implement method: by_name")
 
-    @classmethod
     @abstractmethod
-    def by_id_and_user_id(cls, pet_id: int, user_id: int) -> Dict[bool, List[Pets]]:
+    def by_id_and_user_id(self, pet_id: int, user_id: int) -> Dict[bool, List[Pets]]:
         """Specific case"""
         raise NotImplementedError("Should implement method: by_id_name")
--- src/domain/use_cases/find_user.py
+++ src/domain/use_cases/find_user.py
@@ -6,20 +6,17 @@ from src.domain.models import Users
 class FindUser(ABC):
     """Interface to FindUser use case"""
 
-    @classmethod
     @abstractmethod
-    def by_id(cls, user_id: int) -> Dict[bool, List[Users]]:
+    def by_id(self, user_id: int) -> Dict[bool, List[Users]]:
         """Specific case"""
         raise NotImplementedError("Should implement method: by_id")
 
-    @classmethod
     @abstractmethod
-    def by_name(cls, user_name: str) -> Dict[bool, List[Users]]:
+    def by_name(self, user_name: str) -> Dict[bool, List[Users]]:
         """Specific case"""
         raise NotImplementedError("Should implement method: by_name")
 
-    @classmethod
     @abstractmethod
-    def by_id_and_name(cls, user_id: int, user_name: str) -> Dict[bool, List[Users]]:
+    def by_id_and_name(self, user_id: int, user_name: str) -> Dict[bool, List[Users]]:
         """Specific case"""
         raise NotImplementedError("Should implement method: by_id_name")

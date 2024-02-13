cf981bdcaf86bfb017a2bea847d8fa57c719e8f9
David Shelton <david.faria012@gmail.com>
2024-02-12 22:44:41 -0300

    test(FindPet use case): Add new test cases Find pet by id, but passing no pet_id; Find pet by user id, but passingno user id; Find pet by pet id and user id, but passing no pet id; Find pet by pet id and user id, but passing no user id; Find pet by pet id and user id, but passing no input

--- src/data/find_pet/find_test.py
+++ src/data/find_pet/find_test.py
@@ -5,7 +5,7 @@ from .find import FindPet
 faker = Faker()
 
 
-def test_by_id():
+def test_by_pet_id():
     """Testing Find Pet use case"""
     pet_repo = PetRepositorySpy()
     find_pet = FindPet(pet_repo)
@@ -25,6 +25,28 @@ def test_by_id():
     # Test outputs
     assert response["Success"] is True
     assert response["Data"]
+
+
+def test_by_id_no_pet_id():
+    """Testing Find Pet use case"""
+    pet_repo = PetRepositorySpy()
+    find_pet = FindPet(pet_repo)
+
+    # Fake data
+    attributes = {"pet_id": None}
+
+    # Select pet by id:
+    response = find_pet.by_id(attributes["pet_id"])
+
+    print(pet_repo.select_pet_params)
+    print(response)
+
+    # Test inputs
+    assert not pet_repo.select_pet_params
+
+    # Test outputs
+    assert response["Success"] is False
+    assert not response["Data"]
 
 
 def test_by_user_id():
@@ -49,7 +71,29 @@ def test_by_user_id():
     assert response["Data"]
 
 
-def test_by_id_and_user_id():
+def test_by_user_id_no_user_id():
+    """Testing Find Pet use case"""
+    pet_repo = PetRepositorySpy()
+    find_pet = FindPet(pet_repo)
+
+    # Fake data
+    attributes = {"user_id": None}
+
+    # Select pet by user_id:
+    response = find_pet.by_user_id(attributes["user_id"])
+
+    print(pet_repo.select_pet_params)
+    print(response)
+
+    # Test inputs
+    assert not pet_repo.select_pet_params
+
+    # Test outputs
+    assert response["Success"] is False
+    assert not response["Data"]
+
+
+def test_by_pet_id_and_user_id():
     """Testing Find Pet use case"""
     pet_repo = PetRepositorySpy()
     find_pet = FindPet(pet_repo)
@@ -73,3 +117,75 @@ def test_by_id_and_user_id():
     # Test outputs
     assert response["Success"] is True
     assert response["Data"]
+
+
+def test_by_pet_id_and_user_id_no_pet_id():
+    """Testing Find Pet use case"""
+    pet_repo = PetRepositorySpy()
+    find_pet = FindPet(pet_repo)
+
+    # Fake data
+    attributes = {"pet_id": None, "user_id": faker.random_number(digits=5)}
+
+    # Select pet by user_id:
+    response = find_pet.by_id_and_user_id(attributes["pet_id"], attributes["user_id"])
+
+    print(pet_repo.select_pet_params)
+    print(response)
+
+    # Test inputs
+    assert not pet_repo.select_pet_params
+    assert not pet_repo.select_pet_params
+
+    # Test outputs
+    assert response["Success"] is False
+    assert not response["Data"]
+
+
+def test_by_pet_id_and_user_id_no_user_id():
+    """Testing Find Pet use case"""
+    pet_repo = PetRepositorySpy()
+    find_pet = FindPet(pet_repo)
+
+    # Fake data
+    attributes = {"pet_id": faker.random_number(digits=5), "user_id": None}
+
+    # Select pet by user_id:
+    response = find_pet.by_id_and_user_id(attributes["pet_id"], attributes["user_id"])
+
+    print(pet_repo.select_pet_params)
+    print(response)
+
+    # Test inputs
+    assert not pet_repo.select_pet_params
+    assert not pet_repo.select_pet_params
+
+    # Test outputs
+    assert response["Success"] is False
+    assert not response["Data"]
+
+
+def test_by_pet_id_and_user_id_no_input():
+    """Testing Find Pet use case"""
+    pet_repo = PetRepositorySpy()
+    find_pet = FindPet(pet_repo)
+
+    # Fake data
+    attributes = {
+        "pet_id": None,
+        "user_id": None,
+    }
+
+    # Select pet by user_id:
+    response = find_pet.by_id_and_user_id(attributes["pet_id"], attributes["user_id"])
+
+    print(pet_repo.select_pet_params)
+    print(response)
+
+    # Test inputs
+    assert not pet_repo.select_pet_params
+    assert not pet_repo.select_pet_params
+
+    # Test outputs
+    assert response["Success"] is False
+    assert not response["Data"]
